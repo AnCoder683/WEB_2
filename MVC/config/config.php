308a47afffaca620b3a7aa5e6daa39a612596d75
thuan <thuanann.sama@gmail.com>
2024-05-15 19:29:35 +0700

    re work

--- MVC/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    define("BASE_URL","http://localhost/fashionstoremvc");
-    define("BASE_ASSETS","http://localhost/fashionstoremvc/MVC/assets");
-    define("BASE_VIEWS","http://localhost/fashionstoremvc/MVC/views");
-    define("product_per_page", 3);
-?>
